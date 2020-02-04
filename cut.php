2a8f71e7ed5439105943fe5888d0458fe3d35cba
NSKING.CN <3014311561@qq.com>
2020-02-05 00:28:21 +0800

    Create cut.php
    分割字典文件，以CRC32b前三位分别存储以增加查找效率

---
+++ cut.php
@@ -0,0 +1,11 @@
+<?php
+$input = fopen(intval($argv[1]), "r");//打开未分割的字典
+while(!feof($input))
+{
+   $output = fopen(str_replace("\r\n", "", substr(fgets($input),-5)), "a");\\将字典条目按照CRC32b值前3位进行分别存储，存储进"前三位".txt内
+     echo fgets($input);
+     fwrite($output,fgets($input));
+     fclose($output);
+}
+fclose($input);
+?> 
