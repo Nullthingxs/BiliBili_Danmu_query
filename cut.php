be31cea5071daff3440ab1a9c68a8bed262b1735
NSKING.CN <3014311561@qq.com>
2020-02-05 18:13:34 +0800

    Update cut.php

--- cut.php
+++ cut.php
@@ -1,8 +1,13 @@
+
 <?php
 $input = fopen(intval($argv[1]), "r");//打开未分割的字典
+$dir = './hashtable/';
+if(!is_dir($dir)){
+    mkdir(iconv("UTF-8", "GBK", $dir),0777,true);
+}    
 while(!feof($input))
 {
-   $output = fopen(str_replace("\r\n", "", substr(fgets($input),-5)), "a");//将字典条目按照CRC32b值前3位进行分别存储，存储进"前三位".txt内
+   $output = fopen("$dir".str_replace("\r\n", "",substr(fgets($input),-5)), "a");//将字典条目按照CRC32b值前3位进行分别存储，存储进"前三位".txt内
      echo fgets($input);
      fwrite($output,fgets($input));
      fclose($output);
